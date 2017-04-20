ea87ceaca1cfd509364e1e0d700bd964d41f3b07
BenNoonan1991 <ben.noonan2@gmail.com>
2017-04-20 10:04:12 +0100

    Adds bees and cows to terrible

--- terrible/lib/animal_catalog.rb
+++ terrible/lib/animal_catalog.rb
@@ -6,25 +6,31 @@ class AnimalCatalog
       "meow"
     elsif type == :bird
       "chirp"
+    elsif type == :cow
+      "moo"
+    elsif type == :bee
+      "buzz"
     else
       fail "Unrecognised animal"
     end
   end
 
   def number_of_legs(type)
-    if type == :dog || type == :cat
+    if type == :dog || type == :cat || type == :cow
       4
     elsif type == :bird
       2
+    elsif type == :bee
+      6
     else
       fail "Unrecognised animal"
     end
   end
 
   def number_of_wings(type)
-    if type == :dog || type == :cat
+    if type == :dog || type == :cat || type == :cow
       0
-    elsif type == :bird
+    elsif type == :bird || type == :bee 
       2
     else
       fail "Unrecognised animal"
